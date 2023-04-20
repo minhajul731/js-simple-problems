6316da0056a2a218e06ee4aec8a3a628be8c89f3
showkhin1997 <showkhin15@gmail.com>
2023-04-20 15:18:58 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,13 @@
+// precess 1
+let first = 4;
+let second = 8;
+console.log(first, second);
+// let temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// process 2
+// disctructuring
+[first, second] = [second, first];
+console.log(first, second);
